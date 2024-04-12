69fe5f0b67c8198145ac459151811b20f15c1d94
Robert A. Petit III <robbie.petit@gmail.com>
2024-04-12 02:38:54 +0000

    starter using cookiecutter

---
+++ config/api_router.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+from steamboat.users.api.views import UserViewSet
+
+router = DefaultRouter() if settings.DEBUG else SimpleRouter()
+
+router.register("users", UserViewSet)
+
+app_name = "api"
+urlpatterns = router.urls
